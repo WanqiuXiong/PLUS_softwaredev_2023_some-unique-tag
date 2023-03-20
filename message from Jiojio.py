55d497474dd34b0bb9f29c555292156381d2ba76
Jiojio <127099703+SuperJiojio@users.noreply.github.com>
2023-03-20 16:28:50 +0100

    Gretting form Jiojio.py
    there is nothing here.Save your time and energy

---
+++ message from Jiojio.py
@@ -0,0 +1,5 @@
+Hello Sarah
+life is like a box of chocolate, you never know what you are gonna to get.
+I like the strudel we tried together the other day.
+I would like to go to the traditional Austrian restaurant together with you and Peijing
+Wie gehts
